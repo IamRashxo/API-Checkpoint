336ff367f09d6d2b78990d23f7c8eb68a606e42e
Rashxo <oladeler@gmail.com>
2024-02-29 23:35:09 +0100

    APi Checkpoint

---
+++ user-api/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import UserList from './UserList/UserList';
+import './App.css'; //created with love
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
